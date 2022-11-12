7c1fc1a46249e1a9cb825bb9db45942b81d753a5
George-Gyang <georgegyang3@gmail>
2022-11-12 11:36:12 +0100

    project summary

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,19 @@ import React from "react";
 import Footer from "./footer";
 import Navbar from "./Navbar";
 
-const Home = () =>{
+const Home = () => {
 
-    return(
+    return (
         <>
-        <Navbar />
-        <main className="main">
-            <h1>React Custom and useReducer Hooks Counter App</h1>
-        </main>
-        <Footer/>
+            <Navbar />
+            <main className="main">
+                <h1>React Custom and useReducer Hooks Counter App</h1>
+                <p>
+                    Setting a counter that increment, decrement, setValue, and reset, Implemented using custom hook
+                    and a combination of states with a useReducer. showing 404 page and error boundary testing page
+                </p>
+            </main>
+            <Footer />
         </>
     )
 }
